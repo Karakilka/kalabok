bd068d5d221ed96129a25ac7667bee857bc1707f
Uliana <ulyana_marchenko12345@mail.ru>
2022-11-19 09:08:48 +0300

    priv

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Main
 {
+    //43424234234234
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
